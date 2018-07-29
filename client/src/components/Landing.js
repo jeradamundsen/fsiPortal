b0cd523b8b05119a8a5a36b94f816b665a86d05a
Jerad Amundsen <jerad.amundsen@gmail.com>
2018-07-29 16:56:33 -0600

    new client components

---
+++ client/src/components/Landing.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Landing = ()=>{
+  return(
+    <div style={{textAlign: 'center'}}>
+      <h1>
+        FSI Opertunity Registration
+      </h1>
+      Register An Opportunity
+    </div>
+  )
+}
+
+export default Landing
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,21 +1,30 @@
-const passport =require('passport')
-module.exports = (app)=>{
+const passport = require('passport')
+module.exports = app =>{
   app.get(
     '/auth/google',
      passport.authenticate('google', {
        scope: ['profile', 'email']
      })
-   )
-   app.get(
-     '/auth/google/callback', passport.authenticate('google')
-   )
-   app.get(
-     '/api/current_user', (req,res) => {
+   );
+
+   app.get('/auth/google/callback',
+    passport.authenticate('google'),
+    (req, res)=>{
+      res.redirect('/surveys')
+    }
+
+  )
+
+   app.get('/api/logout', (req,res) => {
+     req.logout();
+     
+
+       res.redirect('/')
+
+   })
+
+   app.get('/api/current_user', (req,res) => {
        res.send(req.user)
      }
    )
-   app.get('/api/logout', (req,res)=>{
-     req.logout()
-     res.send(req.user)
-   })
 }
